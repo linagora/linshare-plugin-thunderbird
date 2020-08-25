cca5406309e958688fbe8abf3b11a5eb40ce734b
Mohamed ABERKIH <maberkih@linagora.com>
2020-08-25 15:21:58 +0200

    removing old assets

--- content/preferences.js
+++
@@ -1,5 +0,0 @@
-Preferences.addAll([
-    { id: "emailpref", type: "string", name: "extensions.linshare.email" },
-    { id: "urlpref", type: "string", name: "extensions.linshare.url" },
-    { id: "messagepref", type: "wstring", name: "extensions.linshare.message" },
-]);
